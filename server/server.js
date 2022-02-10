e7c0162a1d9c703e805263870626e61f399e8632
arghachdas <arghachandra@technoexponent.com>
2022-02-10 20:41:30 +0530

    connection to mongodb and separate out routes and controller

--- server/server.js
+++ server/server.js
@@ -1,1 +1,31 @@
-console.log("server")
+import express from "express"
+import cors from "cors"
+import { readdirSync } from "fs"
+const morgan = require("morgan")
+import mongoose from "mongoose"
+require("dotenv").config()
+
+// create express app
+const app = express()
+
+//db connect
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(`DB connection error: ${err}`))
+
+// app middlewares
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+
+// autoload route
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)))
+
+// route
+app.get("/")
+
+//  port
+const port = process.env.PORT || 8000
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
